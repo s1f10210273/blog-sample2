35fe004251e752077f4d674047e1814e34d58f26
s1f10210273 <s1f102102739@iniad.org>
2024-08-04 01:06:27 +0900

    デザイン修正

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -3,6 +3,8 @@ import parse from "html-react-parser";
 import { getDetail, getList } from "@/libs/microcms";
 import { Suspense } from "react";
 import Loading from "@/app/components/loading";
+import { MdArrowBack, MdArrowUpward } from "react-icons/md";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   const { contents } = await getList();
@@ -31,14 +33,28 @@ export default async function StaticDetailPage({
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 sm:p-8 lg:p-12">
-      <Suspense fallback={<Loading />}>
-        <article className="prose lg:prose-xl mx-auto">
-          <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-          <p className="text-gray-500 mb-6">{time}</p>
-          <div>{parse(post.content)}</div>
-        </article>
-      </Suspense>
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-sky-600 text-white py-4 px-6 flex items-center justify-between">
+        <Link href="/" className="text-white flex items-center">
+          <MdArrowBack className="mr-2" />
+          Back to Articles
+        </Link>
+        <h1 className="text-2xl font-bold">BlogSample</h1>
+      </header>
+      <main className="max-w-4xl mx-auto p-6 sm:p-8 lg:p-12">
+        <Suspense fallback={<Loading />}>
+          <article className="prose lg:prose-xl mx-auto bg-white p-6 rounded-lg shadow-md">
+            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+            <p className="text-gray-500 mb-6">{time}</p>
+            <div>{parse(post.content)}</div>
+          </article>
+        </Suspense>
+        <footer className="fixed bottom-4 right-4">
+          <a href="#top" className="flex items-center text-sky-500">
+            <MdArrowUpward className="text-4xl" />
+          </a>
+        </footer>
+      </main>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,44 @@
 import Link from "next/link";
 import { getList } from "@/libs/microcms";
+import { MdArrowUpward } from "react-icons/md";
 
 export default async function StaticPage() {
   const { contents } = await getList();
 
-  if (!contents || contents.length === 0) {
-    return (
-      <h1 className="text-center text-2xl font-bold mt-10">No contents</h1>
-    );
-  }
-
   return (
-    <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Articles</h1>
-      <ul className="space-y-4">
-        {contents.map((post) => {
-          return (
-            <li
-              key={post.id}
-              className="p-4 border rounded-md shadow hover:shadow-lg transition-shadow duration-300"
-            >
-              <Link href={`${post.id}`} className="text-xl font-semibold">
-                {post.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-sky-600 text-white py-4 text-center">
+        <h1 className="text-2xl font-bold">BlogSample</h1>
+      </header>
+      <main className="max-w-4xl mx-auto p-4" id="top">
+        {contents && contents.length > 0 ? (
+          <>
+            <h1 className="text-3xl font-bold mb-6">Articles</h1>
+            <ul className="space-y-4">
+              {contents.map((post) => (
+                <li
+                  key={post.id}
+                  className="p-4 border rounded-md shadow hover:shadow-lg transition-shadow duration-300"
+                >
+                  <Link
+                    href={`/${post.id}`}
+                    className="text-xl font-semibold flex items-center space-x-2"
+                  >
+                    <span>{post.title}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <h1 className="text-center text-2xl font-bold mt-10">No contents</h1>
+        )}
+      </main>
+      <footer className="fixed bottom-4 right-4">
+        <a href="#top" className="flex items-center text-sky-400">
+          <MdArrowUpward className="text-4xl" />
+        </a>
+      </footer>
     </div>
   );
 }
